33cbd9f8e6c79bd8940df7b7160b4b7e1cb026b9
Chinmay Anand <chinmayanand896@gmail.com>
2025-02-21 15:58:05 +0900

    initiated a game window with hello world

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ use bracket_lib::prelude::*;
 
 struct State {}
 
-fn main() {
-    println!("Hello, world!");
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, world!");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Bird")
+        .build()?;
+    main_loop(context, State {})
 }
